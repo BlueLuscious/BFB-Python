ade1a3cd23fe153d4bfdb6495136b2973eb40602
BlueLuscious <lucio.200152@gmail.com>
2023-07-27 18:32:28 -0300

    Add some validations
    (email, username, password)
    to not overwrite users

---
+++ BFB/helpers/validations.py
@@ -0,0 +1,31 @@
+from Models import User
+
+user = User()
+
+class Validations:
+    @staticmethod
+    def validate_exited_email(userEmail):
+        exitedEmail = User.select().where(User.email == userEmail)
+        if exitedEmail:
+            print("This email already has an account")
+            return False
+        else:
+            return True
+    
+    @staticmethod
+    def validate_exited_username(userUsername):
+        exitedEmail = User.select().where(User.email == userUsername)
+        if exitedEmail:
+            print("This username already exists")
+            return False
+        else:
+            return True
+        
+    @staticmethod
+    def validate_exited_password(userPassword):
+        exitedEmail = User.select().where(User.email == userPassword)
+        if exitedEmail:
+            print("This password already exists")
+            return False
+        else:
+            return True
