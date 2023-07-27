422ef071a5b2586c67152a7a021fe4add6acbfea
BlueLuscious <lucio.200152@gmail.com>
2023-07-27 18:43:02 -0300

    CRUD (Create)

--- BFB/Create.py
+++ BFB/Create.py
@@ -1,12 +1,30 @@
 from peewee import *
-from Models import Person
+from Models import User
+from helpers.validations import Validations
 
 db = SqliteDatabase("db.sqlite")
+user = User()
+validator = Validations()
 
-def create_table():
-    db.create_tables([Person])
+def create_table_user():
+    db.create_tables([User])
     
-def drop_table():
-    db.drop_tables([Person])
+def insert_user():
+    user.email = input("Enter your email: ")
+    user.firstname = input("Enter your first name: ")
+    user.lastname = input("Enter your last name: ")
+    user.username = input("Enter your username: ")
+    user.password = input("Enter your password: ")
+    
+    valdidatedEmail = validator.validate_exited_email(user.email)
+    valdidatedUsername = validator.validate_exited_username(user.username)
+    valdidatedPassword = validator.validate_exited_password(user.password)
 
-drop_table()
+    if valdidatedEmail and valdidatedUsername and valdidatedPassword:
+        user.save()
+    else:
+        return False
+    
+
+create_table_user()
+insert_user()
