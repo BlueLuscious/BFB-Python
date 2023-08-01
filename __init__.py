c13d1e2e4db4c33cf6b219f23438705302e1ee81
BlueLuscious <lucio.200152@gmail.com>
2023-08-01 17:33:24 -0300

    update commads

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@
 # pip freeze or pip list (show all package)
 # python -m venv .venv
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-# virtualenvs\.venv\Scripts\activate.bat
-# virtualenvs\.venv\Scripts\deactivate.bat
+# .venv\Scripts\activate.bat or .venv\Scripts\activate
+# .venv\Scripts\deactivate.bat or .venv\Scripts\deactivate
