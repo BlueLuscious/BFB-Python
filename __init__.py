88cfd8d4d0f704d7eb6349c4cfbd69c5ea3203d7
BlueLuscious <lucio.200152@gmail.com>
2023-08-01 17:28:45 -0300

    add command
    permissions

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 # cd (folder) or (.. (back))
 # pip freeze or pip list (show all package)
 # python -m venv .venv
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 # virtualenvs\.venv\Scripts\activate.bat
 # virtualenvs\.venv\Scripts\deactivate.bat
