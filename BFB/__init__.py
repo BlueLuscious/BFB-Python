722199958c925981d22d398fa1f940c99caf524f
BlueLuscious <lucio.200152@gmail.com>
2023-08-01 16:53:16 -0300

    restart

--- BFB/__init__.py
+++
@@ -1,5 +0,0 @@
-# cd (folder) or (.. (back))
-# pip freeze or pip list (show all package)
-# python -m venv .venv
-# virtualenvs\.venv\Scripts\activate.bat
-# virtualenvs\.venv\Scripts\deactivate.bat
---
+++ __init__.py
@@ -0,0 +1,5 @@
+# cd (folder) or (.. (back))
+# pip freeze or pip list (show all package)
+# python -m venv .venv
+# virtualenvs\.venv\Scripts\activate.bat
+# virtualenvs\.venv\Scripts\deactivate.bat
