ab0b8cf724d15499abbaf055d90a23650eca536a
BlueLuscious <lucio.200152@gmail.com>
2023-08-01 17:39:04 -0300

    update

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@
 # pip freeze or pip list (show all package)
 # python -m venv .venv
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-# .venv\Scripts\activate.bat or .venv\Scripts\activate
-# .venv\Scripts\deactivate.bat or .venv\Scripts\deactivate
+# .venv\Scripts\activate.bat y venv\Scripts\deactivate.bat (cmd)
+# .venv\Scripts\activate y deactivate (python)
+# .venv\Scripts\activate.ps1 y deactivate (powershell)
