e30df6e69deacca1f77ec59ea8d2df9abebc4a29
BlueLuscious <lucio.200152@gmail.com>
2023-08-02 20:23:17 -0300

    create commands.txt

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# cd (folder) or (.. (back))
-# pip freeze or pip list (show all package)
-# python -m venv .venv
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-# .venv\Scripts\activate.bat y venv\Scripts\deactivate.bat (cmd)
-# .venv\Scripts\activate y deactivate (python)
-# .venv\Scripts\activate.ps1 y deactivate (powershell)
