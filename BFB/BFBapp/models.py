013407dc9fa71a1e2fc0e1e7e4cd3088828270d3
BlueLuscious <lucio.200152@gmail.com>
2023-08-02 23:15:29 -0300

    Update BFBapp\models
    Create User(Model)
    Add to Admin
    Migrate the model

--- BFB/BFBapp/models.py
+++ BFB/BFBapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=256)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=100)
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=15)
+    repeat_password = models.CharField(max_length=15)
+    
+    def __str__(self) -> str:
+        return self.firstname, self.lastname
+    
