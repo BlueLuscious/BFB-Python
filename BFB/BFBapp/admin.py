47af12129626f0ff41942947c5c69f579946dd64
BlueLuscious <lucio.200152@gmail.com>
2023-08-03 02:37:19 -0300

    Create models (Teacher & Course)
    Fix: Write a str, not a tuple
    Add to Admin
    Migrate the models

--- BFB/BFBapp/admin.py
+++ BFB/BFBapp/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Course, Teacher
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
     pass
