0be41a77c3dec0e323d447bf227f400916e7739b
BlueLuscious <lucio.200152@gmail.com>
2023-08-13 02:35:40 -0300

    Create urls.py to BFBapp
    And include in BFB (proyect)

--- BFB/BFB/urls.py
+++ BFB/BFB/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from BFBapp import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login, name="login"),
-    path('register/', views.register, name="register"),
+    path('', include('BFBapp.urls'))
 ]
